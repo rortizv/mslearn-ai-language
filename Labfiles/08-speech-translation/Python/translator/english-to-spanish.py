d431b0ebb57a2521550197292a4ba726cb4b49e8
Rafael Ortiz <rafael007@gmail.com>
2024-03-31 10:21:59 -0500

    Translate from english speech to spanish or any language text

--- Labfiles/08-speech-translation/Python/translator/english-to-spanish.py
+++ Labfiles/08-speech-translation/Python/translator/english-to-spanish.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-from datetime import datetime
 import os
 
 # Import namespaces
@@ -18,28 +17,19 @@ def main():
         # Configure translation
         translation_config = speech_sdk.translation.SpeechTranslationConfig(ai_key, ai_region)
         translation_config.speech_recognition_language = 'en-US'
-        translation_config.add_target_language('fr')
-        translation_config.add_target_language('es')
-        translation_config.add_target_language('hi')
+        translation_config.add_target_language('es')  # Set target language to Spanish
         print('Ready to translate from ',translation_config.speech_recognition_language)
 
         # Configure speech
         speech_config = speech_sdk.SpeechConfig(ai_key, ai_region)
 
-        # Get user input
-        targetLanguage = ''
-        while targetLanguage != 'quit':
-            targetLanguage = input('\nEnter a target language\n fr = French\n es = Spanish\n hi = Hindi\n Enter anything else to stop\n').lower()
-            if targetLanguage in translation_config.target_languages:
-                Translate(targetLanguage)
-            else:
-                targetLanguage = 'quit'
-                
+        # Start translation
+        Translate()
 
     except Exception as ex:
         print(ex)
 
-def Translate(targetLanguage):
+def Translate():
     translation = ''
 
     # Translate speech
@@ -48,16 +38,11 @@ def Translate(targetLanguage):
     print("Speak now...")
     result = translator.recognize_once_async().get()
     print('Translating "{}"'.format(result.text))
-    translation = result.translations[targetLanguage]
+    translation = result.translations['es']  # Translate to Spanish
     print(translation)
 
     # Synthesize translation
-    voices = {
-            "fr": "fr-FR-HenriNeural",
-            "es": "es-ES-ElviraNeural",
-            "hi": "hi-IN-MadhurNeural"
-    }
-    speech_config.speech_synthesis_voice_name = voices.get(targetLanguage)
+    speech_config.speech_synthesis_voice_name = "es-ES-ElviraNeural"  # Set Spanish voice
     speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
     speak = speech_synthesizer.speak_text_async(translation).get()
     if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:

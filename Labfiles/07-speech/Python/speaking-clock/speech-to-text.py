7eae3a184678118bbbcc6f80317166141ba12c40
Rafael Ortiz <rafael007@gmail.com>
2024-03-31 09:59:50 -0500

    Any spoken speech to text in console

--- Labfiles/07-speech/Python/speaking-clock/speech-to-text.py
+++ Labfiles/07-speech/Python/speaking-clock/speech-to-text.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-from datetime import datetime
 import os
 
 # Import namespaces
@@ -19,11 +18,9 @@ def main():
         speech_config = speech_sdk.SpeechConfig(ai_key, ai_region)
         print('Ready to use speech service in:', speech_config.region)
         
-
         # Get spoken input
         command = TranscribeCommand()
-        if command.lower() == 'what time is it?':
-            TellTime()
+        print("Transcribed text:", command)
 
     except Exception as ex:
         print(ex)
@@ -36,13 +33,12 @@ def TranscribeCommand():
     speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
     print('Speak now...')
 
-
     # Process speech input
     speech = speech_recognizer.recognize_once_async().get()
-    print(speech.text)
+    #print(speech.text)
     if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
         command = speech.text
-        print(command)
+     #   print(command)
     else:
         print(speech.reason)
         if speech.reason == speech_sdk.ResultReason.Canceled:
@@ -50,29 +46,8 @@ def TranscribeCommand():
             print(cancellation.reason)
             print(cancellation.error_details)
 
-
     # Return the command
     return command
-
-
-def TellTime():
-    now = datetime.now()
-    response_text = 'The time is {}:{:02d}'.format(now.hour,now.minute)
-
-
-    # Configure speech synthesis
-    speech_config.speech_synthesis_voice_name = "en-GB-RyanNeural"
-    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
-    
-
-    # Synthesize spoken output
-    speak = speech_synthesizer.speak_text_async(response_text).get()
-    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
-        print(speak.reason)
-
-
-    # Print the response
-    print(response_text)
 
 
 if __name__ == "__main__":
